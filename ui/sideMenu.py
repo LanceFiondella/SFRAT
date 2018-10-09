f859208f46ec53095e408f301d247953798763e5
Tim  Chase <tchase@umassd.edu>
2018-10-09 13:12:22 -0400

    Added slots

--- ui/sideMenu.py
+++ ui/sideMenu.py
@@ -14,12 +14,18 @@ class SideMenu(QVBoxLayout):
 	def initUI(self):
 		self.button1 = QPushButton("This is the ")
 		self.button2 = QPushButton("Side Menu")
+
+		self.button1.clicked.connect(self.b1Pressed)
+		self.button2.clicked.connect(self.b2Pressed)
+
 		self.addWidget(self.button1)
 		self.addWidget(self.button2)
 		self.addStretch(1)
 
 	def b1Pressed(self):
-		QMessageBox.about(self, "You Pressed Button 1", "Dont Push me bro")
+		log.info("Side Menu B1 Pressed! Doing what b1 does")
+	def b2Pressed(self):
+		log.info("Side Menu B2 Pressed! Doing what b2 does")
 
 
 
