c375fd1ccffee2256701ca4953a9ad135a6f2a86
Tim  Chase <tchase@umassd.edu>
2018-10-11 11:57:15 -0400

    Tabs to spaces

--- main.py
+++ main.py
@@ -10,33 +10,31 @@ from ui.mainWindow import *
 
 
 def main():
-	log.info("Hello From Main")
+    log.info("Hello From Main")
 
-	app = QApplication(sys.argv)
-	ex = App()
-	sys.exit(app.exec_())
+    app = QApplication(sys.argv)
+    ex = App()
+    sys.exit(app.exec_())
 
 
 
 if __name__ == "__main__":
-	# command line arguments
-	parser = argparse.ArgumentParser(description='Software Reliablity Tool')
-	parser.add_argument("-v", '--verbose', action='store_true')
+    # command line arguments
+    parser = argparse.ArgumentParser(description='Software Reliablity Tool')
+    parser.add_argument("-v", '--verbose', action='store_true')
 
-	args = parser.parse_args()
+    args = parser.parse_args()
 
-	if args.verbose:
-		# if using verbose output set logging to display level and message
-		# print anything level debug or higher
-		log.basicConfig(format="%(levelname)s: %(message)s", level=log.DEBUG)
-		log.info("Using Verbose output.")
-	else:
-		# if not using verbose output only brint errors and warnings
-		log.basicConfig(format="%(levelname)s: %(message)s")
+    if args.verbose:
+    # if using verbose output set logging to display level and message
+    # print anything level debug or higher
+    log.basicConfig(format="%(levelname)s: %(message)s", level=log.DEBUG)
+    log.info("Using Verbose output.")
+    else:
+    # if not using verbose output only brint errors and warnings
+    log.basicConfig(format="%(levelname)s: %(message)s")
 
-	main()
-
-
+    main()
 
 
 
@@ -49,4 +47,6 @@ if __name__ == "__main__":
 
 
 
-	#
+
+
+    #
--- ui/sideMenu.py
+++ ui/sideMenu.py
@@ -7,25 +7,25 @@ import logging as log
 
 
 class SideMenu(QVBoxLayout):
-	def __init__(self):
-		super().__init__()
-		self.initUI()
+    def __init__(self):
+    super().__init__()
+    self.initUI()
 
-	def initUI(self):
-		self.button1 = QPushButton("This is the ")
-		self.button2 = QPushButton("Side Menu")
+    def initUI(self):
+        self.button1 = QPushButton("This is the ")
+        self.button2 = QPushButton("Side Menu")
 
-		self.button1.clicked.connect(self.b1Pressed)
-		self.button2.clicked.connect(self.b2Pressed)
+        self.button1.clicked.connect(self.b1Pressed)
+        self.button2.clicked.connect(self.b2Pressed)
 
-		self.addWidget(self.button1)
-		self.addWidget(self.button2)
-		self.addStretch(1)
+        self.addWidget(self.button1)
+        self.addWidget(self.button2)
+        self.addStretch(1)
 
-	def b1Pressed(self):
-		log.info("Side Menu B1 Pressed! Doing what b1 does")
-	def b2Pressed(self):
-		log.info("Side Menu B2 Pressed! Doing what b2 does")
+    def b1Pressed(self):
+        log.info("Side Menu B1 Pressed! Doing what b1 does")
+    def b2Pressed(self):
+        log.info("Side Menu B2 Pressed! Doing what b2 does")
 
 
 
