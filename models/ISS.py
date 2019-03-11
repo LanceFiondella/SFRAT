f7451363f86c24a7a962ffcad1fce18efea6f5d8
Shekar V <vshekar@umassd.edu>
2019-03-11 16:10:19 -0400

    Fixed ISS model

--- models/ISS.py
+++ models/ISS.py
@@ -24,13 +24,13 @@ class ISS(Model):
         """
         super().__init__(*args, **kwargs)
         self.n = len(self.data)
-        self.tn = self.data.FT.iloc[-1] 
+        self.tn = self.data.FT.iloc[-1]
         self.sumT = self.data.FT.sum()
         self.rootFindFunc = RootFind(rootAlgoName=kwargs['rootAlgoName'],
                                      equation=self.MLEeq,
                                      data=self.data)
 
-    def findParams(self, predictPoints): 
+    def findParams(self, predictPoints):
         """
         Find parameters of the model
 
@@ -38,16 +38,16 @@ class ISS(Model):
         """
         print(self.data.FT)
         print(self.n, self.n/sum(self.data.FT), 1.0)
-        sol = scipy.optimize.root(self.MLEeq,[self.n, self.n/sum(self.data.FT), 1.0], options={'maxfev':10000})
+        sol = scipy.optimize.root(self.MLEeq, [self.n, self.n/sum(self.data.FT), 1.0], options={'maxfev':10000})
         print(sol)
-        self.aMLE,self.bMLE,self.cMLE = sol.x
+        self.aMLE, self.bMLE, self.cMLE = sol.x
         self.predict(predictPoints)
         self.MVFVal = np.append(self.MVF(self.aMLE, self.bMLE, self.cMLE, self.data.FT), self.futureFailures)
         self.predictedFailureTimes = np.append(self.data.FT, self.predictedFailureTimes)
         self.FIVal = self.FI(self.aMLE, self.bMLE, self.cMLE,np.append(self.data.FN, self.futureFailures))
         self.MTTFVal = self.MTTF()
 
-    def MVF(self, a, b, c,t):
+    def MVF(self, a, b, c, t):
         """
         Calculates the Mean Value Function (MVF) based on a, b, and c
 
@@ -61,13 +61,13 @@ class ISS(Model):
         return (a*(1-np.exp(-b*t)))/(1+c*np.exp(-b*t))
 
     def MVFPlot(self):
-        return (self.predictedFailureTimes,self.MVFVal[:len(self.predictedFailureTimes)])
+        return (self.predictedFailureTimes, self.MVFVal[:len(self.predictedFailureTimes)])
 
     def MTTFPlot(self):
-    	return (self.predictedFailureTimes,self.MTTFVal[:len(self.predictedFailureTimes)])
+        return (self.predictedFailureTimes, self.MTTFVal[:len(self.predictedFailureTimes)])
 
     def FIPlot(self):
-        return (self.predictedFailureTimes,self.FIVal[:len(self.predictedFailureTimes)])
+        return (self.predictedFailureTimes, self.FIVal[:len(self.predictedFailureTimes)])
 
     def relGrowthPlot(self, interval):
         growth = []
@@ -86,7 +86,7 @@ class ISS(Model):
         self.predictedFailureTimes = np.array(self.predictedFailureTimes)
         self.futureFailures = np.array(futureFailures)
 
-    def FI(self, a, b, c, failureTimes):
+    def FI(self, a, b, c, t):
         """
         Calculates Failure Intensity
 
@@ -97,14 +97,14 @@ class ISS(Model):
         Returns:
             Failure Intensity as numpy array
         """
-        return (a*b*(c+1)*np.exp(b*failureTimes))/((c+np.exp(b*failureTimes))**2)
+        return (a*b*(c+1)*np.exp(b*t))/((c+np.exp(b*t))**2)
 
     def lnL(self, a, b, c):
-    	aMLE = n/((1-np.exp(-b*self.tn))/(1+c*np.exp(-b*self.tn)))
-    	firstTerm = (aMLE*(1-np.exp(-b*self.tn)))/(1+c*np.exp(-b*self.tn))
-    	secondTerm = self.n*np.log(a)+self.n*np.log(b)+self.n*np.log(1+c)
-    	thirdTerm = np.sum(np.log(c+np.exp(b*self.data.FT)))                        
-    	return -firstTerm + secondTerm + b*self.sumT - 2*thirdTerm
+        aMLE = self.n/((1-np.exp(-b*self.tn))/(1+c*np.exp(-b*self.tn)))
+        firstTerm = (aMLE*(1-np.exp(-b*self.tn)))/(1+c*np.exp(-b*self.tn))
+        secondTerm = self.n*np.log(a)+self.n*np.log(b)+self.n*np.log(1+c)
+        thirdTerm = np.sum(np.log(c+np.exp(b*self.data.FT)))                        
+        return -firstTerm + secondTerm + b*self.sumT - 2*thirdTerm
 
     def reliability(self, t, interval):  #Check with Shekar
         """
@@ -124,10 +124,10 @@ class ISS(Model):
     def MTTF(self):  #Check with Shekar #This is for TBF plot on Tab 2
         return np.reciprocal(self.FIVal)
 
-    def finite_model(self): 
+    def finite_model(self):
         return True
 
-    def MLEeq(self,x): 
+    def MLEeq(self, x):
         """
         Represents MLE eqation, used in root finding
 
@@ -138,23 +138,25 @@ class ISS(Model):
             Value of MLE equation
         """
         a, b, c = x
-        aNr = 1-np.exp(-b*self.tn)
-        aDr = c+np.exp(-b*self.tn)
-        aEq = (self.n/a) - (aNr/aDr)
+        aNr = 1-np.exp(b*self.tn)
+        aDr = c+np.exp(b*self.tn)
+        aEq = (self.n/a) + (aNr/aDr)
 
+        if (c+np.exp(b*self.tn))**2 == 0:
+            print("denom 0 for {} {} {}".format(a, b, c))
         bFirstPart = (-a*(1+c)*self.tn*np.exp(b*self.tn))/((c+np.exp(b*self.tn))**2)
         bSecondPart = np.sum((1/b)-((2*self.data.FT*np.exp(b*self.data.FT))/(c+np.exp(b*self.data.FT))) + self.data.FT)
-        bEq = (bFirstPart + bSecondPart)
+        bEq = bFirstPart + bSecondPart
 
-        cFirstPart = (a*(-1+np.exp(b*self.tn))/((c+np.exp(b*self.tn))**2))
+        cFirstPart = a*(-1+np.exp(b*self.tn))/((c+np.exp(b*self.tn))**2)
         cSecondPart = np.sum((-2/(c+np.exp(b*self.data.FT))) + (1/(1+c)))        
-        cEq = (cFirstPart+cSecondPart)
+        cEq = cFirstPart + cSecondPart
         return [aEq, bEq, cEq]
 
-
-
 if __name__ == "__main__":
-    fname = "model_data.xlsx"
-    rawData = pd.read_excel(fname, sheetname='SS4')
-    iss = ISS(rawData, 'newton')
-    iss.findParams()
+    #fname = "model_data.xlsx"
+    fname = "C:/Users/Shekar/Dropbox/NASA OSMA SARP/FY18/reporting/Goddard F2F January 2019/copied/NASA/NASAX.xlsx"
+    rawData = pd.read_excel(fname, sheet_name='NASA1')
+    iss = ISS(data=rawData, rootAlgoName='newton')
+    iss.findParams(1)
+    #print(iss.MLEeq([61.7598, 0.0462066, 67.507]))
