77a0dbbb4378b138abcf91e34c584f5a8b8c1b79
KenanChen3 <43079108+KenanChen3@users.noreply.github.com>
2020-07-07 10:27:55 -0400

    Added GM unit testing script

---
+++ tests/test_GM.py
@@ -0,0 +1,79 @@
+import pytest
+import os, sys, inspect
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0, parentdir)
+from models.GM import GM
+import pandas as pd
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+mylogger = logging.getLogger()
+
+
+def setup_gm():
+    fname = "model_data.xlsx"
+    dataResults = pd.read_excel(fname, sheet_name='GM_BM_Results')
+    sheets = dataResults['Data.set'].to_numpy()
+    D0 = dataResults['D0'].to_numpy()
+    Phi = dataResults['Phi'].to_numpy()
+
+    gm_list = []
+
+    # When Creating a instance of a model class, the DATA class is needed. For some datasets, the 'IF' column is missing which causes an error
+
+    for sheet in sheets:
+        rawData = pd.read_excel(fname, sheet_name=sheet)
+        try:
+            gm = GM(data=rawData, rootAlgoName='bisect')
+            gm.findParams(0)
+        except:
+            gm = None
+        gm_list.append(gm)
+    return [gm_list, D0, Phi]
+
+
+DATA = setup_gm()
+Results_DMLE = []
+Results_PhiMLE = []
+for i in range(0, len(DATA[0])):
+    try:
+        Results_DMLE.append((DATA[0][i].DMLE, DATA[1][i]))
+        Results_PhiMLE.append((DATA[0][i].phiMLE, DATA[2][i]))
+    except:
+        pass
+
+print(Results_DMLE)
+print(Results_PhiMLE)
+@pytest.mark.parametrize("test_input,expected", Results_DMLE)
+def test_gm_d_mle(test_input, expected):
+    mylogger.info('IN TEST')
+    assert abs(test_input - expected) < 10 ** -5
+
+
+@pytest.mark.parametrize("test_input,expected", Results_PhiMLE)
+def test_gm_phi_mle(test_input, expected):
+    assert abs(test_input - expected) < 10 ** -5
+
+
+def test_name(setup_gm):
+    assert setup_gm.name == "Geometric"
+
+
+'''                                                                                                                                               
+def test_jm_n0_mle(setup_jm):                                                                                                                     
+    for i in range(0, len(setup_jm[0])):                                                                                                          
+        assert abs(setup_jm[0][i].N0MLE - setup_jm[1][i]) < 10**-5                                                                                
+
+
+def test_jm_phi_mle(setup_jm):                                                                                                                    
+    assert abs(setup_jm.phiMLE - 3.4966515966450457e-05) < 10**-5                                                                                 
+
+
+def test_jm_mvf_last(setup_jm):                                                                                                                   
+    assert abs(setup_jm.MVFVal[-1] - 135.516034) < 10**-5                                                                                         
+
+
+def test_name(setup_jm):                                                                                                                          
+    assert setup_jm.name == "Jelinski-Moranda"                                                                                                    
+'''
--- tests/test_models.py
+++ tests/test_models.py
@@ -2,3 +2,5 @@ import os
 
 #Running JM Model Test
 os.system('pytest -s test_JM.py')
+
+os.system('pytest -s test_GM.py')
