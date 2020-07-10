ad3ca9433f75ee4c2521d2575c236250a642e6c2
GMZak <36744404+GMZak@users.noreply.github.com>
2020-07-10 14:44:49 -0400

    Testing email notification for Travis CI

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,7 +1,7 @@
 import os
 #Running JM Model Test
-os.system('pytest -s test_JM.py')
-os.system('pytest -s test_GM.py')
+#os.system('pytest -s test_JM.py')
+#os.system('pytest -s test_GM.py')
 
 
-os.system('pytest -s test_GO.py')
+os.system('conda activate DataScience && pytest -s test_GO.py')
