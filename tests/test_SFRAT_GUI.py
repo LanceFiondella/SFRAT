e017313d2dbcd82815639dc19dcc37388658d579
GMZak <36744404+GMZak@users.noreply.github.com>
2020-07-15 00:12:39 -0400

    Travis Changes

---
+++ tests/test_SFRAT_GUI.py
@@ -0,0 +1,26 @@
+import sys
+import unittest
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtTest import QTest
+from PyQt5.QtCore import Qt
+### Changes the curretnt working directory so the imports Work
+import os,sys,inspect
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0,parentdir)
+import ui.mainWindow
+
+'''Testing the SFRAT GUI'''
+app = QApplication(sys.argv)
+form = ui.mainWindow.MainWindow()
+
+def test_defaults():
+    ''' TEst the GUI in it's defaul state'''
+    assert form.title == 'SFRAT'
+    assert form.left == 10
+    assert form.top == 10
+    assert form.width == 1080
+    assert form.height == 720
+
+
+
