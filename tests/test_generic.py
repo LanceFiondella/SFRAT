d5e102f84a2696257f2eeb9d3334140141cd687a
GMZak <36744404+GMZak@users.noreply.github.com>
2020-07-20 14:56:08 -0400

    Travis Changes

--- tests/test_generic.py
+++ tests/test_generic.py
@@ -1,5 +1,8 @@
 import pytest
 import os, sys, inspect
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0,parentdir)
 from core.model import Model
 from core.dataClass import Data
 from models import DSS, GM, GO, ISS, JM, WEI
