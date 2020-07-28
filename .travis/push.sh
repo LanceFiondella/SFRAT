8f78871cf9a91d552956946570b87a830bf5d99b
GMZak <36744404+GMZak@users.noreply.github.com>
2020-07-28 13:11:31 -0400

    Travis Changes

--- .travis/push.sh
+++ .travis/push.sh
@@ -6,14 +6,14 @@ setup_git() {
 }
 
 commit_website_files() {
-  git add -f tests/report.xls
-  git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
+  git add -f report.xls
+  git commit -a -m "Travis build: $TRAVIS_BUILD_NUMBER" -m "[skip ci]"
 }
 
 upload_files() {
   git remote rm origin
   git remote add origin https://${GITHUB_TOKEN}@https://github.com/LanceFiondella/SFRAT.git > /dev/null 2>&1
-  git push origin dev --quiet
+  git push origin dev
 }
 
 
