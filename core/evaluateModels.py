7310f7ab090431839ad70a16b2c5c1ab123fb3bc
steakelum <jsteakelum@gmail.com>
2020-08-17 16:56:55 -0400

    fix PSSE

--- core/evaluateModels.py
+++ core/evaluateModels.py
@@ -10,20 +10,10 @@ class Module:
 		return (2*p - 2*model.lnL())
 
 
-	def PSSE(self, model, data):
-		mvf = model.MVFVal	# includes predicted values
-		n = len(data)
-		k = max(floor(n*self.pctPSSE), 1)
-		failN = list(range(k+1,n))	# inclusive n
-		print(k,n)
-		failT = data[k+1:]
-		return sum((failT[index] - mvf[index])**2 for timeval in failN)
-
-
-	def PSSE(self, model, data):
+	def PSSE(self, model, data):	# R array indexing is 1->N, whereas python 0->N-1
 		n = len(data)
 		k = max(floor(self.pctPSSE*n), 1)
-		failNums = list(range(k,n))
+		failNums = list(range(k+1,n+1))
 		failTimes = data[-len(failNums):]
 		mvfTab = [model.MVF(i) for i in failTimes]
 		return sum((i - mvfTab[idx])**2 for idx, i in enumerate(failNums))
